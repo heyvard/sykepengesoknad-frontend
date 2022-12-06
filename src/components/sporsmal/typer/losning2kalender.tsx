35afe22a397f14d3356ec8636a44859d233df857
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2022-12-06 08:30:37 +0100

    se kalender

--- src/components/sporsmal/typer/losning2kalender.tsx
+++ src/components/sporsmal/typer/losning2kalender.tsx
@@ -1,58 +1,81 @@
-import { BodyLong, Label, TextField } from '@navikt/ds-react'
+import { Label, TextField, ToggleGroup } from '@navikt/ds-react'
 import React, { useState } from 'react'
 import { UNSAFE_DatePicker } from '@navikt/ds-react'
+import dayjs from 'dayjs'
 
 import Vis from '../../vis'
 import { SpmProps } from '../sporsmal-form/sporsmal-form'
-import { tilLesbarDatoMedArstall } from '../../../utils/dato-utils'
+import { tilLesbarPeriodeMedArstall } from '../../../utils/dato-utils'
+import Utvidbar from '../../utvidbar/utvidbar'
 
 export const Losning2Kalender = ({ sporsmal }: SpmProps) => {
-    const [valgtDatoer, setValgteDatoer] = useState<Date[] | undefined>()
+    const [timerProsent, setTimerProsent] = useState<'Timer' | '%'>('Timer')
 
-    const sorterte = valgtDatoer?.sort((a, b) => a.getTime() - b.getTime())
     return (
         <Vis
             hvis={sporsmal.sporsmalstekst}
             render={() => (
-                <div>
+                <div style={{ paddingTop: '2em' }}>
                     <Label as="h2" className="skjema__sporsmal">
-                        Marker hvilke dager du jobbet i denne perioden:
+                        Oppgi total arbeidsmengde du jobbet i denne perioden:
                     </Label>
-
-                    <UNSAFE_DatePicker.Standalone
-                        mode="multiple"
-                        min={0}
-                        fromDate={new Date(sporsmal.min!)}
-                        toDate={new Date(sporsmal.max!)}
-                        onSelect={setValgteDatoer}
-                    />
-
-                    {(valgtDatoer?.length || 0 > 0) && (
-                        <>
-                            <Label as="h2" className="skjema__sporsmal">
-                                Oppgi hvor mye du jobbet på de valgte dagene:
-                            </Label>
-                            <BodyLong as="div" className="redaksjonelt-innhold">
-                                Eksempel 7,5
-                            </BodyLong>
-                        </>
-                    )}
-                    <div style={{ display: 'flex', flexWrap: 'wrap' }}>
-                        {sorterte &&
-                            sorterte.map((d) => {
-                                const formattert = tilLesbarDatoMedArstall(d)
-                                return (
-                                    <TextField
-                                        style={{ width: '133px', marginRight: '1em' }}
-                                        key={formattert}
-                                        label={formattert}
-                                        inputMode="numeric"
-                                        pattern="[0-9]*"
-                                        description="Oppgi timer per dag"
-                                    />
-                                )
-                            })}
+                    <div style={{ display: 'flex' }}>
+                        <TextField style={{ width: '200px' }} label={''} />
+                        <Label
+                            style={{
+                                display: 'flex',
+                                margin: 'auto',
+                                fontWeight: 400,
+                                color: '#595959',
+                                width: '100%',
+                                textAlign: 'left',
+                                paddingLeft: '1em',
+                            }}
+                        >
+                            {timerProsent}
+                        </Label>
                     </div>
+                    <div style={{ display: 'flex' }}>
+                        <ToggleGroup
+                            style={{ marginTop: '1em', marginBottom: '1em', padding: '1.5px' }}
+                            value={timerProsent}
+                            size={'small'}
+                            onChange={(e) => setTimerProsent(e as any)}
+                        >
+                            <ToggleGroup.Item value="Timer">7,5</ToggleGroup.Item>
+                            <ToggleGroup.Item value="%">%</ToggleGroup.Item>
+                        </ToggleGroup>
+                        <Label
+                            style={{
+                                display: 'flex',
+                                margin: 'auto',
+                                fontWeight: 400,
+                                color: '#595959',
+                                width: '100%',
+                                marginTop: '1.5em',
+                                textAlign: 'left',
+                                paddingLeft: '1em',
+                            }}
+                        >
+                            Eksempel: 7,5 timer eller 50 %
+                        </Label>
+                    </div>
+
+                    <Utvidbar
+                        erApen={false}
+                        type="intern"
+                        tittel={
+                            'Se kalender for perioden ' +
+                            tilLesbarPeriodeMedArstall(dayjs(sporsmal.min), dayjs(sporsmal.max))
+                        }
+                    >
+                        <UNSAFE_DatePicker.Standalone
+                            mode="multiple"
+                            min={0}
+                            fromDate={new Date(sporsmal.min!)}
+                            toDate={new Date(sporsmal.max!)}
+                        />
+                    </Utvidbar>
                 </div>
             )}
         />
